b68dc217fe7b02da06b7fa7e4c7b27dfe24a28a1
juliusbenjamins <julius.benjamins1@gmail.com>
2025-09-05 20:12:57 +0200

    Added row/col focus

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 import { act } from 'react-dom/test-utils';
+
+
+const HORIZONTAL = 0;
+const VERTICAL = 1;
 
 export const App = () => {
   // Variables
   const [loading, setLoading] = useState(true);
   const [puzzle, setPuzzle] = useState();
-  const [activeCell, setActiveCell] = useState({ row: 0, col: 0 });
-  const [direction, setDirection] = useState();
+  const [activeCell, setActiveCell] = useState({ row: null, col: null });
+  const [direction, setDirection] = useState(HORIZONTAL); //0 = horizontal, 1=vertical
+  const inputsRef = useRef([]); // om refs naar alle inputs op te slaan
+
   const size = 5;
 
-  const handleLetterInput = (row, col) => {
+  const handleFocus = (row, col) => {
     setActiveCell({ row, col });
-    console.log("Current:", activeCell.row);
+
+    console.log("Current:", activeCell.row, activeCell.col);
   };
+
+  const handleClick = (row, col) => {
+    if (activeCell.row == row && activeCell.col == col) {
+      setDirection(!direction)
+    }
+  }
+
+  // const handleKeyDown = (e, row, col) => {
+  //   let newRow = row;
+  //   let newCol = col;
+
+  //   if (e.key === "ArrowUp") newRow = row - 1;
+  //   if (e.key === "ArrowDown") newRow = row + 1;
+  //   if (e.key === "ArrowLeft") newCol = col - 1;
+  //   if (e.key === "ArrowRight") newCol = col + 1;
+
+  //   console.log("Next:", newRow, newCol);
+  //   setActiveCell({newRow, newCol});
+
+  //   // if (newRow !== row || newCol !== col) {
+  //   //   e.preventDefault(); // standaard cursor beweging voorkomen
+  //   //   const nextInput = inputsRef.current[`${newRow}-${newCol}`];
+  //   //   if (nextInput) nextInput.focus();
+  //   // }
+  // };
 
   useEffect(() => {
     async function fetchData() {
       var puzzleDatabase = require('./db/puzzles.json');
-
-      // console.log(puzzleDatabase.puzzles[0].data)
-
       setPuzzle(puzzleDatabase.puzzles[0].data)
       setLoading(false);
     }
@@ -66,17 +95,26 @@ export const App = () => {
                             <div
                               className='items-center border border-black bg-white'>
                               <input
-                                className={`p-4 sm:p-6 md:px-8 text-center text-3xl uppercase focus:outline-none focus:ring-0 
+                                className={`p-4 sm:p-6 md:px-8 text-center text-3xl uppercase focus:outline-none focus:ring-0                                 
                                   ${activeCell.row == row && activeCell.col == col
-                                    ? "bg-green-300"
-                                    : "bg-white"
+                                    ? "bg-green-300 "
+                                    : "bg-white "
+                                    /* Sets the active cell to green */
+                                  }
+                                  ${(direction == HORIZONTAL && activeCell.row == row && activeCell.col != col) ||
+                                    direction == VERTICAL && activeCell.col == col && activeCell.row != row
+                                    ? "bg-green-100 "
+                                    : "bg-white "
+                                    /* Set active row or column to soft green */
                                   }`}
                                 size="1"
                                 type="text"
                                 maxLength="1"
                                 name="myInput"
-                                onFocus={() => handleLetterInput(row, col)}
+                                onFocus={() => handleFocus(row, col)}
+                                onClick={() => handleClick(row, col)}
                                 onBlur={() => console.log("Previous:", row, col)}
+                              // onKeyDown={(e) => handleKeyDown(e, row, col)}
                               />
                             </div>
                           </div>
